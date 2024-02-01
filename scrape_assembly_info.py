7c0239a06ea2896c160f1cef3b71991f33f1365d
Hirokazu Chiba <hchiba1@gmail.com>
2024-02-02 06:52:03 +0900

    Remove comments in scrape_assembly_info.py

--- scrape_assembly_info.py
+++ scrape_assembly_info.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-
-# The variables that we want to parse are:
-# Assembly method, Genome coverage, and Sequencing technology, and Submitter
-# Written by Ashlyn Powell and Paul Frandsen
 import sys
 import csv
 import requests
